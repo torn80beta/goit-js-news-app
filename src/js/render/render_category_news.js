9167901b77e0e1d868cbb1103def1b98fd673272
Valera Kamelkov <tahaburrent@gmail.com>
2023-03-28 14:54:06 +0200

    new attempt

--- src/js/render/render_category_news.js
+++ src/js/render/render_category_news.js
@@ -1,8 +1,10 @@
 const newsGalleryEl = document.querySelector('.gallery__cards-list');
 import { FavoriteStorage, ReadStorage } from '../localStorage';
+import { onCardClick, onReadClick } from '../onCardClick';
 const favoriteStorage = new FavoriteStorage();
 const readStorage = new ReadStorage();
-
+const newsgallery = document.querySelector('.gallery__cards-list');
+import moment from 'moment/moment';
 export function renderCategoryMarkup(news) {
   const markup = news
     .map(
@@ -18,7 +20,6 @@ export function renderCategoryMarkup(news) {
         },
         index
       ) => {
-
         if (index <= 8) {
           let activeClass = '';
           let activeText = '';
@@ -72,7 +73,7 @@ export function renderCategoryMarkup(news) {
                 <img class="photo" src="${image}" alt="${alt}" loading="lazy" />
 		    </div>
             <div class="card-category">${section.toLowerCase()}</div>
-            <button type="button" class="${activeClass}">Add to favorite <span><svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
+            <button type="button" class="${activeClass}">${activeText}<span><svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
     <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4"
         stroke-width="2.2857"
         d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z">
@@ -94,10 +95,9 @@ export function renderCategoryMarkup(news) {
   newsGalleryEl.innerHTML = markup;
 }
 
+newsgallery.addEventListener('click', onReadClick);
 
-newsgallery.addEventListener('click', readStorage.onReadClick);
-
-newsgallery.addEventListener('click', favoriteStorage.onCardClick);
+newsgallery.addEventListener('click', onCardClick);
 
 const cardUrls = document.querySelectorAll('.card-url');
 cardUrls.forEach(cardUrl => {
@@ -105,4 +105,3 @@ cardUrls.forEach(cardUrl => {
     cardUrl.parentElement.classList.add('opacity');
   });
 });
-
--- src/js/render/render_pop_news.js
+++ src/js/render/render_pop_news.js
@@ -1,9 +1,10 @@
 import { normalizeImage, normalizeAlt, normalizeData } from '../normalize';
 import { FavoriteStorage, ReadStorage } from '../localStorage';
+import { onCardClick, onReadClick } from '../onCardClick';
 const favoriteStorage = new FavoriteStorage();
 const readStorage = new ReadStorage();
 const newsGalleryEl = document.querySelector('.gallery__cards-list');
-
+const newsgallery = document.querySelector('.gallery__cards-list');
 export function renderMostPopMarkup(news) {
   console.log(news);
   const markup = news
@@ -26,18 +27,14 @@ export function renderMostPopMarkup(news) {
         let activeText = '';
         let backdropRead = '';
         let readText = '';
-        if (
-          favoriteStorage.hasNews({
-            url,
-          })
-        ) {
+        if (favoriteStorage.hasNews(url)) {
           activeText = 'Remove from favorite';
           activeClass = 'favorite-button__activ';
         } else {
           activeText = 'Add to favorite';
           activeClass = 'add-to-favorite';
         }
-        if (readStorage.hasNews({ url })) {
+        if (readStorage.hasNews(url)) {
           backdropRead = ' opacity';
           readText = '<span class="news__read-status">Already read</span>';
         }
@@ -46,7 +43,7 @@ export function renderMostPopMarkup(news) {
                 <img class="photo" src="${data.image}" alt="${data.alt}" loading="lazy" />
 		    </div>
             <div class="card-category">${data.category}</div>
-            <button type="button" class="${activeClass}">${activeText} <span><svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
+            <button type="button" class="${activeClass}"> ${activeText} <span><svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
     <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4"
         stroke-width="2.2857"
         d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z">
@@ -65,9 +62,9 @@ export function renderMostPopMarkup(news) {
 
   newsGalleryEl.innerHTML = markup;
 }
-newsgallery.addEventListener('click', readStorage.onReadClick);
+newsgallery.addEventListener('click', onReadClick);
 
-newsgallery.addEventListener('click', favoriteStorage.onCardClick);
+newsgallery.addEventListener('click', onCardClick);
 
 const cardUrls = document.querySelectorAll('.card-url');
 cardUrls.forEach(cardUrl => {
