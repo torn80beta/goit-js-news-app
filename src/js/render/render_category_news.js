c6599a874cc5d783ea052962e74d6106afe7dcb6
emmabaka <bakaemma88@gmail.com>
2023-03-28 20:46:34 +0300

    new commit

--- src/js/render/render_category_news.js
+++ src/js/render/render_category_news.js
@@ -23,6 +23,7 @@ export function renderCategoryMarkup(news) {
         let activeText = '';
         let backdropRead = '';
         let readText = '';
+        let iconClass = '';
         if (
           favoriteStorage.hasNews({
             url,
@@ -30,9 +31,11 @@ export function renderCategoryMarkup(news) {
         ) {
           activeText = 'Remove from favorite';
           activeClass = 'favorite-button__activ';
+          iconClass = 'news-icon';
         } else {
           activeText = 'Add to favorite';
           activeClass = 'add-to-favorite';
+          iconClass = 'active-news-icon';
         }
         if (readStorage.hasNews({ url })) {
           backdropRead = ' opacity';
@@ -58,7 +61,7 @@ export function renderCategoryMarkup(news) {
                 <img class="photo" src="${image}" alt="${alt}" loading="lazy" />
 		    </div>
             <div class="card-category">${section.toLowerCase()}</div>
-            <button type="button" class="${activeClass}">Add to favorite <span><svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
+            <button type="button" class="${activeClass}">Add to favorite <span><svg class="item-news__block-icon ${iconClass}" width="16" height="16" viewBox="0 0 37 32">
     <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4"
         stroke-width="2.2857"
         d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z">
